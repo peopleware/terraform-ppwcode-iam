60664264f460bc901539c381588a31fc6485ffab
Adriaan Peeters <Adriaan_Peeters@peopleware.be>
2023-11-28 11:24:09 +0100

    AllowIndividualUserToSeeAndManageTheirOwnAccountInformation iam:GetAccessKeyLastUsed

--- main.tf
+++ main.tf
@@ -35,6 +35,7 @@ data "aws_iam_policy_document" "AllowIndividualUserToSeeAndManageTheirOwnAccount
       "iam:GetLoginProfile",
       "iam:ListAccessKeys",
       "iam:UpdateAccessKey",
+      "iam:GetAccessKeyLastUsed",
       "iam:UpdateLoginProfile",
     ]
 
